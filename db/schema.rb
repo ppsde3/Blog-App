fc7c7447824d82f6e50ac8e07a529f3f181dec37
ppsde1 <89243473+ppsde1@users.noreply.github.com>
2021-11-13 17:34:33 +0530

    Comment Model Added.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_10_073745) do
+ActiveRecord::Schema.define(version: 2021_11_11_090703) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,12 +18,17 @@ ActiveRecord::Schema.define(version: 2021_11_10_073745) do
     t.integer "user_id"
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+  end
+
   create_table "comments", force: :cascade do |t|
-    t.text "description"
-    t.integer "user_id"
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_comments_on_user_id"
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "users", force: :cascade do |t|
